55ce02974aec4b231341032b03c9e31ca47cb2f5
Salohiddin Usmonaliyev <usmonaliyevsalohiddin@outlook.com>
2025-06-18 16:20:09 +0500

    update (top students in dashboard)

--- tracker/views.py
+++ tracker/views.py
@@ -1,28 +1,34 @@
-from itertools import count
-
 from django.db.models import Count, Q
 from django.shortcuts import render
+from datetime import date
 
-from .models import *
-
-from datetime import datetime
-
-top_students = Student.objects.annotate(
-    total=Count('attendance'),
-    absent=Count('attendance', filter=Q(attendance__status=False))
-).filter(
-    total__gt=5,
-    absent=0
-)
-
-absent_students = Student.objects.annotate(total=Count('attendance', filter=Q(attendance__status=False))).filter(attendance__time=datetime.today())
+from .models import Student, Teacher, Course, Attendance
 
 def index(request):
+    students = Student.objects.annotate(
+        total=Count('attendance'),
+        total_lessons=Count('attendance'),
+        absent=Count('attendance', filter=Q(attendance__status=False)),
+        present=Count('attendance', filter=Q(attendance__status=True))
+    ).filter(
+        total__gt=0,
+        absent__lt=2
+    )
+
+    absent_students = Student.objects.annotate(
+        absents_today=Count('attendance', filter=Q(
+            attendance__status=False,
+            attendance__time__date=date.today()
+        ))
+    ).filter(
+        absents_today__gt=0
+    )
+
     data = {
-        "students": Student.objects.all(),
+        "students": students,  # Already annotated
         "teachers": Teacher.objects.all(),
-        "top_students": top_students,
         "courses": Course.objects.all(),
         "absent_students": absent_students,
+        "lessons": Attendance.objects.all(),  # or .filter(...) if needed
     }
     return render(request, 'index.html', data)
