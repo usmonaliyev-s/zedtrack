e361d190146398e63e4bdb8fd13c7df6fb0eae6a
Salohiddin Usmonaliyev <usmonaliyevsalohiddin@outlook.com>
2025-06-21 14:56:09 +0500

    add a new student

--- tracker/views.py
+++ tracker/views.py
@@ -1,10 +1,11 @@
 from django.db.models import Count, Q, ExpressionWrapper, F
 from django.db.models.fields import FloatField
-from django.db.models.functions import TruncDate
-from django.shortcuts import render
+from django.db.models.functions import TruncDate, NullIf
+from django.shortcuts import render, redirect
 from datetime import date
+from django.utils import timezone
 
-from .models import Student, Teacher, Course, Attendance
+from .models import *
 
 def index(request):
     students = Student.objects.annotate(
@@ -82,18 +83,16 @@ def index(request):
     return render(request, 'index.html', data)
 
 def students_list(request):
-
     students = Student.objects.annotate(
         total=Count('attendance'),
         present=Count('attendance', filter=Q(attendance__status=True)),
     ).annotate(
         attendance_rate=ExpressionWrapper(
-            100.0 * F('present') / F('total'),
+            100.0 * F('present') / NullIf(F('total'), 0),
             output_field=FloatField()
         )
-    ).filter(
-        total__gt=0  # Avoid division by zero
     )
+
     data = {
         "students": students,
     }
@@ -109,8 +108,26 @@ def teachers_list(request):
     return render(request, "teachers_list.html", data)
 
 def courses_list(request):
-    courses = Course.objects.all()
+    courses = Course.objects.annotate(num_students=Count('student', distinct=True))
     data = {
         "courses": courses,
     }
     return render(request, "courses_list.html", data)
+
+
+def add_student(request):
+    data = {
+        "courses": Course.objects.all(),
+    }
+    if request.method == "POST":
+        print(request.POST.get('gender'))
+        Student.objects.create(
+            first_name=request.POST.get('first_name'),
+            last_name=request.POST.get('last_name'),
+            gender=request.POST.get('gender'),
+            course_id=request.POST.get('course'),
+            registration_date=timezone.now().date()
+        )
+        return redirect('/students/')
+
+    return render(request, "add_student.html", data)
