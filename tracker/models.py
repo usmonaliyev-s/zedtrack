6a7d55db65a6faafa686b3cc347be21f3b2fc16d
Salohiddin Usmonaliyev <usmonaliyevsalohiddin@outlook.com>
2025-07-02 03:40:01 -0700

    update

--- tracker/models.py
+++ tracker/models.py
@@ -35,7 +35,7 @@ class Student(models.Model):
     last_name = models.CharField(max_length=100)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
-    registration_date = models.DateField(null=True, blank=True)
+    registration_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
