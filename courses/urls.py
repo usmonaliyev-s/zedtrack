afdce35562b596fe448c08469f9f5d00bd54448e
Salohiddin Usmonaliyev <usmonaliyevsalohiddin@outlook.com>
2025-07-07 08:22:01 +0500

    update

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,8 @@ from .views import *
 urlpatterns = [
     path('list/', courses_list, name='course-list'),
     path('add/', add_course, name='add-course'),
+    path('edit/<int:id>/', edit_course, name='edit-course'),
+    path('delete/<int:id>/confirmation/', delete_confirmation_course, name='delete-confirmation-course'),
+    path('delete/<int:id>/', delete_course, name='delete-course'),
+    path('details/<int:id>/', course_details, name='course-details'),
 ]
