e1122e8c6996834e0528b60f66a290c9c685dabe
Salohiddin Usmonaliyev <usmonaliyevsalohiddin@outlook.com>
2025-07-27 20:02:02 +0500

    update

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ from attendance_tracker.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard, name='dashboard'),
+    path('<str:a>/<str:b>/', dashboard, name='dashboard'),
+    path('<int:c>/', dashboard, name='dashboard'),
     path('index/', index, name='index'),
     path('courses/', include('courses.urls')),
     path('teachers/', include('teachers.urls')),
