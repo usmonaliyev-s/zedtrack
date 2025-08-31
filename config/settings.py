fb654984a8f06929cbad2fd2952d7a777a6c0028
Salohiddin Usmonaliyev <usmonaliyevsalohiddin@outlook.com>
2025-08-31 16:02:17 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,14 @@ ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "password1*", "password2*"]
 
 # Instead of ACCOUNT_EMAIL_VERIFICATION
 ACCOUNT_EMAIL_VERIFICATION = "none"  # can be "optional" or "mandatory"
-
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.mail.me.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "usmonaliyev_s@icloud.com"   # your real email
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "usmonaliyev_s@icloud.com"
 
 LANGUAGE_CODE = 'en-us'
 
